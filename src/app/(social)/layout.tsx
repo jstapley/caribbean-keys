1e331a1c158f0d1d4f0b47d2aed7ccdf7c4ac230
Jeff Stapley <jeff.stapley@live.ca>
2026-08-29 11:08:23 -0400

    Move social landing to route group to exclude header/footer

---
+++ src/app/(social)/layout.tsx
@@ -0,0 +1,13 @@
+// @ts-nocheck
+import type { ReactNode } from "react"
+import "@/app/globals.css"
+
+export default function SocialRootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
